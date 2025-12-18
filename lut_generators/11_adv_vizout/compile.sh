aad91a3d4d54e4be4a05a9244ada9ad866cada61
piotrcurious <78758456+piotrcurious@users.noreply.github.com>
2025-12-18 02:40:15 +0000

    updated to llvm

--- lut_generators/11_adv_vizout/compile.sh
+++ lut_generators/11_adv_vizout/compile.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-g++ -std=c++17 -O3 -march=native -flto -funroll-loops -pthread -o adv_vizout_spectral_01 adv_vizout_spectral_01.cpp
+#g++ -std=c++17 -O3 -march=native -flto -funroll-loops -pthread -o adv_vizout_spectral_01 adv_vizout_spectral_01.cpp
+clang++ -std=c++17 -O3 -march=native -flto=thin -funroll-loops -pthread \
+  -mllvm -enable-ml-inliner=release \
+  -fuse-ld=lld \
+  -o adv_vizout_spectral_01 adv_vizout_spectral_01.cpp
